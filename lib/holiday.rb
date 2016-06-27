a51161af37ed158c1097ab7feb1240c086450930
ckshei <cks856@gmail.com>
2016-06-27 13:24:51 -0400

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,6 +1,8 @@
 require 'pry'
 
 def second_supply_for_fourth_of_july(holiday_hash)
+
+  holiday_hash[:summer][:fourth_of_july][1]
   # given that holiday_hash looks like this:
   # {
   #   :winter => {
@@ -21,6 +23,9 @@ def second_supply_for_fourth_of_july(holiday_hash)
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
+  holiday_hash[:winter].each do |holiday, supplies|
+    supplies << supply
+  end
   # holiday_hash is identical to the one above 
   # add the second argument, which is a supply, to BOTH the 
   # Christmas AND the New Year's arrays
@@ -29,19 +34,25 @@ end
  
 
 def add_supply_to_memorial_day(holiday_hash, supply)
+  holiday_hash[:spring][:memorial_day] << supply
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
   
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
+  # binding.pry
+  holiday_hash[season] = {
+    holiday_name => supply_array
+  }
+  holiday_hash
   # code here
   # remember to return the updated hash
   
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-  # return an array of all of the supplies that are used in the winter season
+  holiday_hash[:winter].values.flatten
   
 end
 
@@ -53,12 +64,28 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  
+
+  holiday_hash.each do |season, hash|
+    puts "#{season.capitalize}:"
+    hash.each do |holiday, supplies|
+      puts "  #{holiday.to_s.split("_").map(&:capitalize).join(" ")}: #{supplies.join(', ')}"
+      end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
+  array_of_keys = []
+  holiday_hash.each do |season, holiday|
+    holiday.each do |holiday_name, supplies_array|
+      supplies_array.each do |supplies|
+        if supplies == "BBQ"
+          array_of_keys << holiday_name
+          # binding.pry
+        end
+      end
+    end
+  end
+  return array_of_keys
 
 end
 
